4e111bb1594bb0963130d7fe2c639204f4729d3b
Hyungyu Park <phg3430@gmail.com>
2025-04-09 18:20:07 +0900

    push

--- docker/start.sh
+++ docker/start.sh
@@ -8,10 +8,13 @@ if [ ! -f $XAUTH ]; then
     xauth nlist $DISPLAY | sed -e 's/^..../ffff/' | xauth -f $XAUTH nmerge -
     echo "[+] Xauthority file created at $XAUTH"
 fi
-
+ 
 # Allow local docker X access
 xhost +local:docker
 
 # Start docker-compose
 echo "[+] Starting forest_localization container..."
-docker-compose up --build
+docker-compose build
+echo "[+] Done starting container..."
+echo "[+] Attaching forest_localization container..."
+docker-compose up -d
